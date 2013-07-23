015eb16d4f9a23e7174129175f8d53a968fddaca
Michael Smith <smithatlanta@gmail.com>
2013-07-23 11:05:21 -0400

    added npm back

--- buildvm.sh
+++ buildvm.sh
@@ -21,7 +21,7 @@ if [ $2 ]; then
 	fi
 fi
 
-echo "Update aptitude with ppa's added above."
+echo "Updating aptitude's repo's."
 sudo apt-get update >> log
 
 echo "Upgrading Ubuntu to the latest of everything."
@@ -31,12 +31,14 @@ echo "Installing all the stuff needed for node."
 sudo apt-get install -y --force-yes python-software-properties python g++ make >> log
 
 if [ $latestnode = 'true' ]; then
+	echo "Using latest Node version"
 	sudo add-apt-repository -y ppa:chris-lea/node.js
 else
-	echo "b"	
+	echo "Using latest Node-legacy version"
 	sudo add-apt-repository -y ppa:chris-lea/node.js-legacy
 fi
 
+echo "Updating aptitude's repo's."
 sudo apt-get update >> log
 
 echo "Installing node."
@@ -44,6 +46,9 @@ sudo apt-get install -y --force-yes nodejs >> log
 
 echo "Outputting node version."
 node –v >> log
+
+echo "Installing npm."
+sudo apt-get install -g npm >> log
 
 echo "Installing node-gyp."
 sudo npm install -g node-gyp >> log
@@ -62,7 +67,7 @@ echo "America/New_York" | sudo tee /etc/timezone
 echo "Reconfiguring tzdata to use changed timezone file."
 sudo dpkg-reconfigure --frontend noninteractive tzdata
 
-echo "Setting file limits"
-echo '* soft nofile 10000'  >> /etc/security/limits.conf
-echo '* hard nofile 10000'  >> /etc/security/limits.conf
+echo "Setting file limits stuff"
+echo '*  soft  nofile  10000'  >> /etc/security/limits.conf
+echo '*  hard  nofile  10000'  >> /etc/security/limits.conf
 echo 'session required pam_limits.so' >> /etc/pam.d/su
